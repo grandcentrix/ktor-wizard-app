6056ad67ccbf30a4f9c964b1a8c08feeda82ed89
Andressa <andressa.marques-fernandes@grandcentrix.net>
2024-04-15 11:03:57 +0200

    status page for 404 and 401 status code

--- src/main/kotlin/net/grandcentrix/backend/plugins/StatusPage.kt
+++ src/main/kotlin/net/grandcentrix/backend/plugins/StatusPage.kt
@@ -1,8 +1,11 @@
 package net.grandcentrix.backend.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureStatusPage() {
@@ -11,6 +14,29 @@ fun Application.configureStatusPage() {
     }
 
     install(StatusPages) {
+        exception<Exception> { call, cause ->
+            when (cause) {
+                is AuthorizationException -> call.respondTemplate(
+                    "error.ftl",
+                    mapOf("errorMessage" to "Error 403. Forbidden.")
+                )
 
+                is UnauthorizedException -> call.respondRedirect("/login")
+
+                else -> call.respondTemplate(
+                    "error.ftl",
+                    mapOf("errorMessage" to "500: Server error - $cause")
+                )
+            }
+        }
+
+        status(HttpStatusCode.NotFound) { call, _ ->
+            call.respondTemplate(
+                "error.ftl",
+                mapOf("errorMessage" to "Oops! It wasn't possible to find the page, or it doesn't exist."))
+        }
     }
 }
+
+//implements a custom exception class
+class AuthorizationException(override val message: String?): Exception()
---
+++ src/main/resources/templates/error.ftl
@@ -0,0 +1,4 @@
+<#import "_layout.ftl" as layout />
+<@layout.base>
+    <h2>${errorMessage}</h2>
+</@layout.base>
