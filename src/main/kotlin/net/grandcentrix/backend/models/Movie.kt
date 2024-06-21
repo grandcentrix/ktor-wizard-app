f80162f6f07ff45c7f52dc86061fa016d63bbf07
Andressa <andressa.marques-fernandes@grandcentrix.net>
2024-06-21 12:14:03 +0200

    converted to nullable values

--- src/main/kotlin/net/grandcentrix/backend/models/Movie.kt
+++ src/main/kotlin/net/grandcentrix/backend/models/Movie.kt
@@ -8,18 +8,19 @@ data class Movie(
     val id: String = String(), //FIXME
     val title: String,
     @SerialName("box_office")
-    val boxOffice: String,
-    val budget: String,
-    val cinematographers: List<String>,
-    val directors: List<String>,
-    val screenwriters: List<String>,
+    val boxOffice: String?,
+    val budget: String?,
+    val cinematographers: List<String>?,
+    val directors: List<String>?,
+    val screenwriters: List<String>?,
     @SerialName("poster")
-    val posterUrl: String,
-    val producers: List<String>,
-    val rating: String,
+    val posterUrl: String?,
+    val producers: List<String>?,
+    val rating: String?,
     @SerialName("release_date")
-    val releaseDate: String,
-    val duration: String,
-    val slug: String,
-    val trailer: String
+    val releaseDate: String?,
+    @SerialName("running_time")
+    val duration: String?,
+    val slug: String?,
+    val trailer: String?
 )
